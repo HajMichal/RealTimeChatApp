2a336aa74172d3d485f5d01d588ee81c2cad4a9d
Michal Haj <michalhaj2003@gmail.com>
2023-04-30 12:10:11 +0200

    removed .env

--- frontend/src/api/axiosCreate.tsx
+++ frontend/src/api/axiosCreate.tsx
@@ -2,7 +2,6 @@ import axios from "axios";
 
 export const axiosCreate = axios.create({
     baseURL: import.meta.env.VITE_BACKEND_URL,
-    // baseURL: "https://realtimechatapp-production-9eac.up.railway.app",
     headers: {
       "Content-Type": "application/json",
     },
