b54b428bc367289c5ddb9f0cd528bb4212985156
Michal Haj <michalhaj2003@gmail.com>
2023-09-09 14:53:29 +0200

    [feat]: Created vercel.json file, moving host of my backend side from railway to vercel

--- backend/app/routers/register.ts
+++ backend/app/routers/register.ts
@@ -6,21 +6,19 @@ const router = Router();
 
 router.post("/register", async (req: Request, res) => {
   const { name, email, password } = req.body;
-  if (name.length < 3) throw new Error("Short name")
+  if (name.length < 3) throw new Error("Short name");
   try {
-    const user = await registerUser(name, email, password)
+    const user = await registerUser(name, email, password);
     createAccessToken(user, res);
   } catch (err) {
-      if ((err as Error)?.message === "Already exists") {
-        res.status(409).json({ message: "Already exists" });
-      } 
-      else if ((err as Error)?.message === "Short name"){
-        res.status(400).json({ message: "Short name" })
-      } 
-      else {
-        res.sendStatus(500);
-      }
+    if ((err as Error)?.message === "Already exists") {
+      res.status(409).json({ message: "Already exists" });
+    } else if ((err as Error)?.message === "Short name") {
+      res.status(400).json({ message: "Short name" });
+    } else {
+      res.sendStatus(500);
     }
+  }
 });
 
 export default router;
